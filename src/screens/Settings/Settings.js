5fadbcbd48ff3a5d11911244e701d7d7c1e6db60
Darren <darrenzhang200027@gmail.com>
2020-10-10 16:23:03 -0400

    added settings page

--- src/screens/Settings/Settings.js
+++ src/screens/Settings/Settings.js
@@ -4,9 +4,57 @@ import styles from './styles';
 import { firebase } from '../../firebase/config'
 
 const Settings = () => {
-    return(
-        <View>
-            <Text>yo</Text>
+
+    const DATA = [
+        {
+            img: user,
+            title: 'My account',
+            content: 'Acount details and profile'
+        },
+        {
+            img: user,
+            title: 'Security',
+            content: 'Security options & controls'
+        },
+        {
+            img: user,
+            title: 'Privacy',
+            content: 'Information collected & usage'
+        },
+        {
+            img: user,
+            title: 'Communication',
+            content: 'Emails, invites and notifications'
+        },
+        {
+            img: user,
+            title: 'Help & Support',
+            content: 'FAQ & help desk'
+        }, {
+            img: user,
+            title: 'Log out',
+            content: 'Log out of your account'
+        },
+
+    ]
+
+
+    const renderItem = ({ item }) => (
+        <Item img={item.img} title={item.title} content={item.content} />
+    );
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.header}>Settings</Text>
+
+            <FlatList
+                data={DATA}
+                renderItem={renderItem}
+                keyExtractor={item => item.title}
+            />
+
+
+
         </View>
     )
 }
