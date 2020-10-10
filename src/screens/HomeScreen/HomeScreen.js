74fa5ae53acd2b2c2688d26026ced60bc483ce80
probeta <33081197+ProBeta1@users.noreply.github.com>
2020-10-10 18:26:59 +0530

    home screen is at home

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,13 +1,50 @@
 import React, { useEffect, useState } from 'react'
-import { FlatList, Keyboard, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { FlatList, Keyboard, Text, TextInput, TouchableOpacity, View, Image } from 'react-native'
 import styles from './styles';
 import { firebase } from '../../firebase/config'
+import upload from "../../pics/upload.png";
 
 export default function HomeScreen(props) {
 
+    const [title, setTitle] = useState("");
+    const [des, setDes] = useState("");
+
+    const uploadImage = () => {
+
+    }
+
     return (
         <View style={styles.container}>
-            <Text>Hello</Text>
+            <Text style={styles.title}>Write down about something good that happened today</Text>
+
+            <Text style={styles.subt}>It can be anything - someone helped, nice weather, promotion...</Text>
+
+            <TextInput
+                style={styles.inputT}
+                placeholder='Title'
+                placeholderTextColor="#aaaaaa"
+                onChangeText={(text) => setTitle(text)}
+                value={title}
+                underlineColorAndroid="transparent"
+                autoCapitalize="none"
+            />
+
+            <TextInput
+                style={styles.inputD}
+                placeholder='Description'
+                placeholderTextColor="#aaaaaa"
+                onChangeText={(text) => setDes(text)}
+                value={des}
+                underlineColorAndroid="transparent"
+                autoCapitalize="none"
+            />
+
+            <TouchableOpacity style={styles.img} onPress={uploadImage}>
+                <Image source={upload} style={styles.imageUpIcon} />
+            </TouchableOpacity>
+
         </View>
     )
 }
+
+console.disableYellowBox = true;
--- src/screens/HomeScreen/styles.js
+++ src/screens/HomeScreen/styles.js
@@ -3,6 +3,48 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center'
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 25,
+        backgroundColor: 'white'
     },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
+    subt: {
+        fontSize: 18,
+        color: '#8E8E8E',
+        textAlign: 'center',
+        marginTop: 10,
+        marginBottom: 20
+    },
+    inputT: {
+        height: 35,
+        borderRadius: 10,
+        width: '90%',
+        backgroundColor: '#E2E2E2',
+        padding: 5,
+        textAlign: 'center'
+
+    },
+    inputD: {
+        height: '30%',
+        width: '90%',
+        borderRadius: 10,
+        overflow: 'hidden',
+        backgroundColor: '#E2E2E2',
+        marginTop: 20,
+        marginBottom: 10,
+        padding: 5,
+        textAlign: 'center'
+    },
+    img: {
+    },
+    imageUpIcon: {
+        width: 100,
+        height: 100,
+        resizeMode: 'cover'
+    }
 })
